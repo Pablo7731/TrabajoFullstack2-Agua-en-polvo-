1bdbba8efe0a3861d52a8aca1e1e84dcdb0cf1ad
Pablo Gatica <Pablo.gatica.v15@gmail.com>
2026-09-04 00:12:39 -0400

    Implementacion del js, una alerta para los botones de agregar al carrito y comprar ahora

---
+++ script.js
@@ -0,0 +1,25 @@
+const carrito = []
+
+const botonesAgregar = document.querySelectorAll(".card-body button, #agua-polvo button");
+
+botonesAgregar.forEach((boton) =>{
+    boton.addEventListener('click', () =>{
+        const contenedor = boton.closest(".card-body") || boton.closest("article");
+
+        if (contenedor){
+            const titulo = contenedor.querySelector("h3");
+
+            if (titulo){
+                let nombreProducto = titulo.textContent.trim();
+
+                if (nombreProducto === "¿Qué es el Agua en Polvo?"){
+                    nombreProducto = "Agua en polvo";
+                }
+
+                carrito.push(nombreProducto)
+
+                alert(`¡${nombreProducto} se ha añadido al carrito!`)
+            }
+        }
+    });
+});
